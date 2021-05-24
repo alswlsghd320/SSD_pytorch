1f12e73f6606f3755f7b7e7f41f1ec33f1680486
chaeyeongyoon <ui88g216@gamil.com>
2021-05-24 22:03:00 +0900

    iou

--- utils/default_box_utils.py
+++ utils/default_box_utils.py
@@ -1,5 +1,57 @@
 import torch
 import math
+def cxcy_to_xyxy(cxcy):
+    """
+    Convert bbox from (cx, cy, w, h) to (xmin, ymin, xmax, ymax)
+
+    :param cxcy:bbox coordinates having center-size coordinates, a tensor of size(n_boxes, 4)
+    :return: bbox in (xmin, ymin, xmax, ymax) coordinates
+    """
+    return torch.cat([cxcy[..., :2] - (cxcy[..., 2:]/2),  cxcy[..., :2] + (cxcy[..., 2:]/2)], -1)
+
+
+def iou(gt_boxes, predicted_boxes):
+    """Return intersection-over-union (Jaccard index) of boxes.
+        each box has (cx, cy, w, h) coordinates
+
+    Args:
+        gt_boxes (n1, 4): ground truth boxes.
+        predicted_boxes (n2, 4): predicted boxes.
+
+    Returns:
+        iou (N): IoU values.
+    """
+    xyxy_gt_boxes = cxcy_to_xyxy(gt_boxes)
+    xyxy_predicted_boxes = cxcy_to_xyxy(predicted_boxes)
+    overlap_left_top = torch.max(xyxy_gt_boxes[..., :2], xyxy_predicted_boxes[..., :2])
+    overlap_right_bottom = torch.min(xyxy_gt_boxes[..., 2:], xyxy_predicted_boxes[..., 2:])
+    overlap_area = (overlap_right_bottom[..., 0] - overlap_left_top[..., 0]) * \
+                   (overlap_right_bottom[..., 1] - overlap_left_top[..., 1])
+
+    gt_area = gt_boxes[..., 2] * gt_boxes[..., 3]
+    pred_area = predicted_boxes[..., 2] * predicted_boxes[..., 3]
+    return overlap_area / (gt_area + pred_area - overlap_area + 1e-5)
+
+def assign_priors(gt_boxes, gt_labels, priors_boxes, iou_threshold=0.5):
+    """
+    Assign ground truth boxes and targets to priors
+    change labels of negative boxes to 0(BG label)
+
+    :param gt_boxes (num_targets, 4):ground truth boxes
+    :param gt_labels(num_targets):
+    :param priors_boxes (num_priors, 4): priors' coordinates (cx, cy, w, h)
+    :return:
+        boxes(num_priors, 4) : real values for priors
+        labels(num_priors): labels for priors
+    """
+    ious = iou(gt_boxes.unsqueeze(0), priors_boxes.unsqueeze(1)) # 모든 prior box들과 gt box 하나의 iou 계산해
+    # [[(gtbox0,prior0 iou), (gtbox1, prior0 iou), (gtbox2, prior0 iou), ... ]
+    # [(gtbox0,prior1 iou), (gtbox1, prior1 iou), (gtbox2, prior1 iou), ... ]...]
+
+    best_target_per_prior, best_target_per_prior_index = ious.max(1)
+    best_prior_per_target, best_prior_per_target_index = ious.max(0)
+    #  to make sure every target has a prior assigned
+
 
 def hard_negative_mining(loss, gt_labels, neg_pos_ratio):
     """
@@ -22,3 +74,10 @@ def hard_negative_mining(loss, gt_labels, neg_pos_ratio):
     _, orders = indexes.sort(dim=1) # indices를 오름차순으로 다시 sort하면 orders는 원래 loss 텐서의 내림차순에서의 순서가 들어가게 된다.
     neg_mask = orders < num_neg # 내림차순에서의 순서가 negative개수 한계보다 작은 위치 True
     return torch.logical_or(neg_mask, pos_mask)
+
+if __name__ == '__main__':
+    t = torch.Tensor([[1, 2, 3, 4], [5, 6, 7, 8], [9, 10, 11, 12]])
+    p = torch.Tensor([[0, 0, 0, 0], [100, 100, 100, 100], [0, 0, 0, 0], [100, 100, 100, 100]])
+    # print(t.unsqueeze(0), p.unsqueeze(1))
+    # print(torch.min(t.unsqueeze(0)[..., 2:], p.unsqueeze(1)[..., :2]))
+    print(iou(t.unsqueeze(0), p.unsqueeze(1)))
